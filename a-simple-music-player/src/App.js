4b15cc6eb23f333d0713cc860487048a6c9eadd0
Eli Goodric <erg2598@tamu.edu>
2025-10-24 14:15:56 -0500

    added literally everything

---
+++ a-simple-music-player/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "./components/Box";
+
+function App() {
+  return (
+    <div className="bg-gray-100 flex justify-center items-center min-h-screen">
+      <Box />
+    </div>
+  );
+}
+
+export default App;
