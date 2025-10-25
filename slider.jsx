a229fa9d974fb16dd3401e385b0a1d10c5ad990f
olaney14 <meatinloafform@gmail.com>
2025-10-24 19:02:28 -0500

    oops

--- slider.jsx
+++
@@ -1,5 +0,0 @@
-<div style={{width: '100%', height: '100%', position: 'relative', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
-    <div style={{width: 290, height: 5, left: 0, top: 5, position: 'absolute', background: 'rgba(217, 217, 217, 0.50)', borderRadius: 25}} />
-    <div style={{width: 50, height: 5, left: 0, top: 5, position: 'absolute', background: '#D9D9D9', borderRadius: 25}} />
-    <div style={{width: 15, height: 15, left: 44, top: 0, position: 'absolute', background: '#D9D9D9', boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)', borderRadius: 9999}} />
-</div>
