f4c6ad79bce7ea066ec9d7b165ef8b68a43e4b5f
ricoooo84 <r.liucallanta@gmail.com>
2025-10-31 18:09:49 -0500

    Loop button
    Added loop button to App.js,  index.css, and added loopbutton.js to components folder

--- a-simple-music-player/src/App.js
+++ a-simple-music-player/src/App.js
@@ -2,9 +2,24 @@ import React from "react";
 import { Box } from "./components/Box";
 
 function App() {
+  // loop button stuff
+  const [isLooping, setIsLooping] = useState(false);
+  const audioRef = useRef(null);
+  const handleLoopChange = (newState) => {
+      setIsLooping(newState);
+      if (audioRef.current) {
+        audioRef.current.loop = newState; 
+      }
+      console.log(newState ? "Loop ON" : "Loop OFF"); // temporary: only for testing
+    };
+    
   return (
     <div className="bg-gray-100 flex justify-center items-center min-h-screen">
       <Box />
+
+      <LoopButton onLoopChange={handleLoopChange} />
+            <p>Loop is {isLooping ? "ON" : "OFF"}</p> {/*temporary indicator of loop on/off*/}
+
     </div>
   );
 }
