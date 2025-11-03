50317adc34fbab9144545881fc86293afb3c66c7
Conor Wang <57606575+conorwang8@users.noreply.github.com>
2025-11-02 18:09:00 -0600

    Create songs.ts

---
+++ components/songs.ts
@@ -0,0 +1,33 @@
+export type Song = {
+  title: string;
+  artist: string;
+  file: string;
+  cover: string;
+};
+
+export const songs: Song[] = [
+  {
+    title: "5150",
+    artist: "Van Halen",
+    file: "/music/5150.mp3",
+    cover: "/covers/5150.png",
+  },
+  {
+    title: "good4u",
+    artist: "Olivia Rodrigo",
+    file: "/music/good4u.mp3",
+    cover: "/covers/good4u.jpg",
+  },
+  {
+    title: "Goodnight",
+    artist: "HalaCG x Axii",
+    file: "/music/goodnight.mp3",
+    cover: "/covers/goodnight.png",
+  },
+  {
+    title: "Totentanz",
+    artist: "Franz Liszt",
+    file: "/music/totentanz.mp3",
+    cover: "/covers/totentanz.jpg",
+  },
+];
