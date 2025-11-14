1a4ef6586c3d1975f3763892b175c879f4f8d5c0
Eli Goodric <erg2598@tamu.edu>
2025-11-14 13:19:23 -0600

    added ngrok functionality

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['.ngrok-free.dev'] // Allows all *.ngrok-free.dev subdomains
+  },
   plugins: [
     react({
       babel: {
